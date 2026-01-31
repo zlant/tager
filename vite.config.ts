6a664e75b133f638f5401e9f00baefa2456b4e1d
zlant <zetx16@gmail.com>
2026-01-31 23:38:56 +0300

    Add github deploy workflow

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  base: process.env.BASE_PATH || '/',
   plugins: [react()],
   server: {
     host: '127.0.0.1', // Explicitly set host to '127.0.0.1'
