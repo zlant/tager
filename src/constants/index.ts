8f6f0314983a2228d291ee6f3c2109a39252641a
zlant <zetx16@gmail.com>
2026-02-03 21:08:12 +0300

    Update APP_VERSION to use version from package.json for better maintainability

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,4 +1,6 @@
-export const APP_VERSION = '0.1.0'
+import packageJson from '../../package.json'
+
+export const APP_VERSION = packageJson.version
 export const EDITOR_TAG = `tager ${APP_VERSION}`
 
 export const STORAGE_KEYS = {
